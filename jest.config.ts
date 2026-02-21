8b7cb09adf388c0bba8b88af9d96da77785f674b
revdev <mail@revdev.me>
2026-02-21 23:50:59 +0100

    fix jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-module.exports = {
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+const config: JestConfigWithTsJest = {
   testEnvironment: 'node',
   modulePathIgnorePatterns: ['<rootDir>/build'],
   preset: 'ts-jest',
@@ -7,3 +8,5 @@ module.exports = {
     '^.+.tsx?$': ['ts-jest', {}],
   },
 };
+
+export default config;
